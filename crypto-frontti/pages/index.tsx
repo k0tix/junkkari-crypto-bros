4c3122b3200a15562716598a8168b84adaca3ceb
Otto Pirttimaa <otto.pirttimaa@gmail.com>
2022-11-05 11:38:32 +0200

    feat: use better style library, add dockerfile and maybe deploy to gcp

--- crypto-frontti/pages/index.tsx
+++ crypto-frontti/pages/index.tsx
@@ -1,52 +1,85 @@
-import Head from 'next/head'
 import { useState } from 'react'
-import { Button, Panel, Placeholder } from 'rsuite'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { Switch, Table, useTheme } from '@nextui-org/react';
+import { useTheme as useNextTheme } from 'next-themes'
+
+const columns = [
+  {
+    key: "currency",
+    label: "CURRENCY",
+  },
+  {
+    key: "role",
+    label: "ROLE",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+];
+const rows = [
+  {
+    key: "1",
+    currency: "BTC",
+    role: "CEO",
+    status: "Active",
+  },
+  {
+    key: "2",
+    currency: "ETH",
+    role: "Technical Lead",
+    status: "Paused",
+  },
+  {
+    key: "3",
+    currency: "Dogecoin",
+    role: "Senior Developer",
+    status: "Active",
+  },
+  {
+    key: "4",
+    currency: "Joku muu",
+    role: "Community Manager",
+    status: "Vacation",
+  },
+];
 
 export default function Home() {
-  const [items, setItems] = useState<number[]>([4, 3, 2, 1]);
-  const [parent] = useAutoAnimate();
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
 
   return (
     <div>
-      <Head>
-        <title>CryptoChimp</title>
-      </Head>
+      The current theme is: {type}
+      <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+      />
 
-      <div className="hero">
-        <h1 className="title" style={{ color: 'whiteSmoke' }}>CryptoChimp</h1>
-        <p className="description" style={{ color: 'grey' }}>
-          Ugala bugala, crazy crypto insight asdasdasdasdasdasdasd
-          <br />
-        </p>
-        <Button appearance="primary" onClick={event => {
-          setItems([...items].sort());
-        }}>
-          {'Click me ;)'}
-        </Button>
-      </div>
-
-      <ul ref={parent as any} style={{ listStyleType: 'none' }}>
-        {items.map(i => (<li key={i}><Panel header="Panel title" bordered style={{ marginTop: '2em' }}>
-          {i}
-          <Placeholder.Paragraph />
-        </Panel></li>))}
-      </ul>
-
-      <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-        padding: 50px;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-    `}</style>
+      <Table
+        bordered
+        shadow={false}
+        selectionMode="multiple"
+        aria-label="Example static bordered collection table"
+        css={{
+          height: "auto",
+          minWidth: "100%",
+        }}
+      >
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column key={column.key}>{column.label}</Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body items={rows}>
+          {(item: any) => (
+            <Table.Row key={item.key}>
+              {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
     </div>
   )
+
 }
